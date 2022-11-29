da8aa610877f3ffc080bc0f50ae9801b737f2336
Raghav Saini <99887889+ravsa0001@users.noreply.github.com>
2022-11-29 18:55:18 +0530

    Update motion_detector.py
    Code to detect the motion

--- motion_detector.py
+++ motion_detector.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 
 first_frame = None
-# List when any moving object appear
 motion_list = [ None, None ]
 # Time of movement
 time = []
@@ -14,15 +13,18 @@ video = cv2.VideoCapture("check.mp4")
 while True:
     check, frame = video.read()
     
-    # motion = 0
+    # Converting color image to gray_scale then to Gaussian blur 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
     if first_frame is None:
         first_frame = gray
         continue
+
+    # Difference between first_frame and current_frame
     diff_frame = cv2.absdiff(first_frame, gray)
 
+    # If change is greater than 30 it will show white color(255)
     thresh_frame = cv2.threshold(diff_frame, 50, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
@@ -30,7 +32,7 @@ while True:
     cnts,_ = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 1000:
             continue
         motion = 1
 
@@ -38,22 +40,14 @@ while True:
         # making rectangle around the moving object
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-    # Appending status of motion
-    motion_list.append(motion)
-
-    motion_list = motion_list[-2:]
-
-    if motion_list[-1] == 1 and motion_list[-2] == 0:
-        time.append(datetime.now())
-
-    if motion_list[-1] == 0 and motion_list[-2] == 1:
-        time.append(datetime.now())
-
-    # cv2.imshow("Gray Frame", gray)
-    # cv2.imshow("Difference Frame", diff_frame)
+    # Displaying image in gray_scale
+    cv2.imshow("Gray Frame", gray)
+    # Displaying the difference in the current_frame to first_frame
+    cv2.imshow("Difference Frame", diff_frame)
+    # Displaying black and white frame 
     cv2.imshow("Threshold Frame", thresh_frame)
-    
-    # cv2.imshow("Color Frame", frame)
+    # Displaying color frame 
+    cv2.imshow("Color Frame", frame)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
